73cbda7cc0cd1ee8ab0a9966074d2576d97e3e80
Ankit Charolia <ankitcharolia@gmail.com>
2023-02-05 19:00:02 +0100

    add S3 features: logging and versioning

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -2,12 +2,40 @@ locals {
   s3_bucket_data = yamldecode(file("./etc/s3-buckets.yaml")) 
 }
 
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "this" {
   for_each = { for bucket in local.s3_bucket_data.buckets : bucket.name => bucket }
 
-  bucket = each.value.name
+  bucket  = each.value.name
 
   tags = {
     Name        = each.value.name
   }
+}
+
+# Bucket ACL
+resource "aws_s3_bucket_acl" "this" {
+  for_each = { for bucket in local.s3_bucket_data.buckets : bucket.name => bucket }
+
+  bucket = each.value.name
+  acl    = "private"
+}
+
+# Bucket Versioning
+resource "aws_s3_bucket_versioning" "this" {
+  for_each = { for bucket in local.s3_bucket_data.buckets : bucket.name => bucket }
+
+  bucket = each.value.name
+
+  versioning_configuration {
+    status      = try(each.value.versioning ? "Enabled" : "Disabled", "Disabled")
+  }
+}
+
+# Bucket logging
+resource "aws_s3_bucket_logging" "this" {
+  for_each = { for bucket in local.s3_bucket_data.buckets : bucket.name => bucket if bucket.logging }
+
+  bucket = each.value.name
+  target_bucket = each.value.name
+  target_prefix = "log/"
 }
