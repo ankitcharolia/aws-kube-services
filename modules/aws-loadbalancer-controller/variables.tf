2b03c4577e4bd4e8b2ad47b1eaa38484912297b0
Ankit Charolia <ankitcharolia@gmail.com>
2023-03-18 19:23:49 +0100

    add aws-loadbalancer-controller module

---
+++ modules/aws-loadbalancer-controller/variables.tf
@@ -0,0 +1,18 @@
+variable "target_revision" {
+  type        = string
+  description = "Helm chart version or git branch/tag for git hosted charts"
+}
+
+variable "cluster_identity_oidc_issuer_url" {
+  type        = string
+  description = "The OIDC Identity issuer for the cluster."
+}
+
+variable "cluster_identity_oidc_issuer_arn" {
+  type        = string
+  description = "The OIDC Identity issuer ARN for the cluster that can be used to associate IAM roles with a service account."
+}
+
+variable "project" {
+  type = string
+}
